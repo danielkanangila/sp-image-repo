9e0ed204234ed0a5c21069de310a36d841cabfec
danielkanangila <daniel.kanangila@gmail.com>
2020-12-20 22:25:25 -0500

    added image repository model

---
+++ image_repo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ImageRepoConfig(AppConfig):
+    name = 'image_repo'
---
+++ image_repo/models.py
@@ -0,0 +1,27 @@
+import os
+from django.db import models
+from accounts.models import User
+from django.utils import timezone
+
+# helper function to build the files name
+
+
+def upload_to(instance, filename):
+    now = timezone.now()
+    base, extension = os.path.splitext(filename.lower())
+    milliseconds = now.microsecond // 1000
+    return f"images/{now:%Y%m%d%H%M%S}{milliseconds}{extension}"
+
+
+class Permissions(models.Model):
+    type = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True, default=None)
+
+
+class ImageRepository(models.Model):
+    caption = models.TextField(null=True, blank=True, default=None)
+    image_url = models.ImageField(upload_to=upload_to)
+    image_type = models.CharField(
+        max_length=50, null=True, blank=True, default=None)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permissions, on_delete=models.PROTECT)
